c2230d65cf82816be2ca4a66a619e2a6425c8982
n3la88 <mari.montab@gmail.com>
2024-10-15 09:33:40 -0600

    Solidity Variable Types

---
+++ SimpleStorage.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+contract SimpleStorage {
+    bool public isTrue;
+    uint public uintNumber;
+    int public intNumber;
+    string public name;
+    address public myAccount;
+    
+}
